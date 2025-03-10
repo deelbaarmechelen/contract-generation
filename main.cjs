11cc048fe524eb858bbc102691c9cbe739353c58
Bernard Butaye <bernard@deelbaarmechelen.be>
2025-03-10 21:18:17 +0100

    add addendum template

--- main.cjs
+++ main.cjs
@@ -17,81 +17,92 @@ log.info('Starting contract generator');
 if (require('electron-squirrel-startup')) app.quit();
 
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 1280,
-		height: 720,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: true,
-			preload: path.join(__dirname, 'preload.js'),
-		},
-	});
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-	win.loadFile('src/index.html');
+  win.loadFile('src/index.html');
 
-	// Open the DevTools.
-	// win.webContents.openDevTools();
+  // Open the DevTools.
+  // win.webContents.openDevTools();
 };
 
 async function handleFileOpen() {
-	const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory'] });
-	if (!canceled) {
-		return filePaths[0];
-	}
+  const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory'] });
+  if (!canceled) {
+    return filePaths[0];
+  }
 }
 const renderAsync = util.promisify(carbone.render);
 
 async function handleRenderPdf(event, data) {
-	log.info('Rendering PDF with data:', data);
-	const extension = 'pdf';
-	// const extension = 'odt'
-	var options = {
-		convertTo: extension,
-	};
-	var templatePath = path.join(__dirname, 'resources', 'template-ontlening.odt');
-	// template file path input
-	try {
-		const result = await renderAsync(templatePath, data, options);
-		const pathDirectory = data.generationInfo.path.trim().length === 0 ? '.' : data.generationInfo.path;
-		let fileName = data.contractNumber.trim().length === 0 ? 'contract' : data.contractNumber;
-		fileName += '.' + extension;
-		const filePath = path.join(pathDirectory, fileName);
-		fs.writeFileSync(filePath, result);
-		// FIXME: when converting to PDF, a 'busy' error is thrown at exit (probably from libreoffice?)
-		// process.exit(); // to kill automatically LibreOffice workers
-		const fileUrl = url.pathToFileURL(filePath);
-		log.info('path: ' + url.fileURLToPath(fileUrl));
-		return fileUrl.href;
-	} catch (err) {
-		log.error('Error generating PDF:', err);
-		throw err;
-	}
+  log.info('Rendering PDF with data:', data);
+  const extension = 'pdf';
+  // const extension = 'odt'
+  var options = {
+    convertTo: extension,
+  };
+  var templateName = data.isExtension ? 'template-addendum.odt' : 'template-ontlening.odt';
+  var templatePath = path.join(__dirname, 'resources', templateName);
+  // template file path input
+  try {
+    const result = await renderAsync(templatePath, data, options);
+    const pathDirectory = data.generationInfo.path.trim().length === 0 ? '.' : data.generationInfo.path;
+    let fileName = data.contractNumber.trim().length === 0 ? 'contract' : data.contractNumber;
+    if (data.isExtension) {
+      const today = new Date();
+      const formattedDate = formatDateYYYYMMDD(today);
+      fileName += '-addendum-' + formattedDate;
+    }
+    fileName += '.' + extension;
+    const filePath = path.join(pathDirectory, fileName);
+    fs.writeFileSync(filePath, result);
+    const fileUrl = url.pathToFileURL(filePath);
+    log.info('path: ' + url.fileURLToPath(fileUrl));
+    return fileUrl.href;
+  } catch (err) {
+    log.error('Error generating PDF:', err);
+    throw err;
+  }
+}
+
+function formatDateYYYYMMDD(date) {
+  const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2); // Add leading zero if needed
+  const day = ('0' + date.getDate()).slice(-2); // Add leading zero if needed
+  return `${year}${month}${day}`;
 }
 
 app.whenReady().then(() => {
-	ipcMain.handle('dialog:openFile', handleFileOpen);
-	//ipcMain.on('generateDoc', renderDoc)
-	ipcMain.handle('generatePdf', handleRenderPdf);
-	createWindow();
+  ipcMain.handle('dialog:openFile', handleFileOpen);
+  ipcMain.handle('generatePdf', handleRenderPdf);
+  createWindow();
 
-	app.on('activate', () => {
-		log.info('activate event triggered');
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
+  app.on('activate', () => {
+    log.info('activate event triggered');
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on('window-all-closed', () => {
-	//process.exit(); // to kill automatically LibreOffice workers
+  // FIXME: when converting to PDF, a 'busy' error is thrown at exit (probably from libreoffice?)
+  //process.exit(); // to kill automatically LibreOffice workers
 
-	if (process.platform !== 'darwin') {
-		try {
-			converter.exit(() => {
-				log.info('Carbone converter exited');
-				log.info('Quitting contract generator');
-				app.quit();
-			});
-		} catch (err) {
-			log.error('Error closing Carbone:', err);
-		}
-	}
+  if (process.platform !== 'darwin') {
+    try {
+      converter.exit(() => {
+        log.info('Carbone converter exited');
+        log.info('Quitting contract generator');
+        app.quit();
+      });
+    } catch (err) {
+      log.error('Error closing Carbone:', err);
+    }
+  }
 });
